bef68c82edc6740b0cf0bdb8cdca4052c2199d8d
Martin Pilný <martin.pilnas@email.cz>
2025-10-08 12:13:01 +0200

    parse classes

---
+++ NNPTPZ1/ComplexNumber.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNPTPZ1
+{
+    public class ComplexNumber
+    {
+        public double Real { get; set; }
+        public float Imaginari { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ComplexNumber)
+            {
+                ComplexNumber x = obj as ComplexNumber;
+                return x.Real == Real && x.Imaginari == Imaginari;
+            }
+            return base.Equals(obj);
+        }
+
+        public readonly static ComplexNumber Zero = new ComplexNumber()
+        {
+            Real = 0,
+            Imaginari = 0
+        };
+
+        public ComplexNumber Multiply(ComplexNumber b)
+        {
+            ComplexNumber a = this;
+            // aRe*bRe + aRe*bIm*i + aIm*bRe*i + aIm*bIm*i*i
+            return new ComplexNumber()
+            {
+                Real = a.Real * b.Real - a.Imaginari * b.Imaginari,
+                Imaginari = (float)(a.Real * b.Imaginari + a.Imaginari * b.Real)
+            };
+        }
+        public double GetAbS()
+        {
+            return Math.Sqrt(Real * Real + Imaginari * Imaginari);
+        }
+
+        public ComplexNumber Add(ComplexNumber b)
+        {
+            ComplexNumber a = this;
+            return new ComplexNumber()
+            {
+                Real = a.Real + b.Real,
+                Imaginari = a.Imaginari + b.Imaginari
+            };
+        }
+        public double GetAngleInDegrees()
+        {
+            return Math.Atan(Imaginari / Real);
+        }
+        public ComplexNumber Subtract(ComplexNumber b)
+        {
+            ComplexNumber a = this;
+            return new ComplexNumber()
+            {
+                Real = a.Real - b.Real,
+                Imaginari = a.Imaginari - b.Imaginari
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"({Real} + {Imaginari}i)";
+        }
+
+        internal ComplexNumber Divide(ComplexNumber b)
+        {
+            // (aRe + aIm*i) / (bRe + bIm*i)
+            // ((aRe + aIm*i) * (bRe - bIm*i)) / ((bRe + bIm*i) * (bRe - bIm*i))
+            //  bRe*bRe - bIm*bIm*i*i
+            var tmp = this.Multiply(new ComplexNumber() { Real = b.Real, Imaginari = -b.Imaginari });
+            var tmp2 = b.Real * b.Real + b.Imaginari * b.Imaginari;
+
+            return new ComplexNumber()
+            {
+                Real = tmp.Real / tmp2,
+                Imaginari = (float)(tmp.Imaginari / tmp2)
+            };
+        }
+    }
+}
